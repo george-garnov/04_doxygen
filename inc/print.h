00584ae0345a838c1143143f7de167c51a4df697
Georgy Garnov <george.garnov@gmail.com>
2018-09-26 22:50:07 +0300

    Добавил tuple

--- inc/print.h
+++ inc/print.h
@@ -2,43 +2,159 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <tuple>
 
-template <bool condition, typename Type>
-struct EnableIf;
- 
-template <typename Type>
-struct EnableIf<true, Type>
+
+// /**
+//  * Проверка на std::tuple.
+//  */
+template <typename> struct is_tuple: std::false_type {};
+
+template <typename ...T> struct is_tuple<std::tuple<T...>>: std::true_type {};
+
+template<typename Type> constexpr bool type_is_tuple()
 {
-    using type = Type;
-};
+  if (is_tuple<Type>::value)  return true;
+  
+  return false;
+}
+
 
 void print(...)
 {
-    //
+    std::cout << "undefined type";
 }
 
-
+// специализация для целочисленных типов
 template <typename T>
-typename EnableIf<std::is_integral<T>::value, void>::type
+typename std::enable_if<std::is_integral<T>::value, void>::type
 print(T value)
 {
-  size_t n = sizeof(T);
-  while(n)
+  size_t size = sizeof(T);
+  while(size)
   {
-    n--;
-    std::cout << ((value >> n*8) & 0xFF);
-    if (n) std::cout << ".";
+    size--;
+    std::cout << ((value >> size*8) & 0xFF);
+    if (size) std::cout << ".";
   }
-  std::cout << std::endl;
 }
 
+// специализация для std::string
+template<typename T>
+typename std::enable_if<std::is_same<T, std::string>::value, void>::type
+print(const T &value)
+{
+  std::cout << value;
+}
+
+// специализация для контейнеров
 template <typename T>
-decltype(begin(std::declval<T>()), end(std::declval<T>()), void())
+typename std::enable_if<std::is_same<T, std::vector<typename T::value_type>>::value ||
+                        std::is_same<T, std::list<typename T::value_type>>::value, void>::type
 print(T container)
 {
+  std::string split = "";
   for(auto value : container)
   {
+    std::cout << split;
     print(value);
+    split = ",";
   }
-  std::cout << std::endl;
+}
+
+namespace tpl
+{
+/*
+ * Главная роль здесь у шаблона класса iterate_tuple.
+    
+ * Первый параметр шаблона класса iterate_tuple имеет тип int (index).
+ * Значение этого параметра используется функцией get, 
+ * которая "достает" из кортежа элемент по указанной позиции.
+    
+ * Мы рекурсивно сдвигаем позицию (уменьшаем index на 1) и таким образом
+ * перемещаемся по кортежу.
+    
+ * Когда значение индекса становится равно 0, рекурсия завершается,
+ * благодаря частичной специализации для индекса равного 0.
+    
+ * При этом есть особый случай, когда индекс равен -1. Он соответствует 
+ * ситуации, когда кортеж не содержит ни одного элемента.
+ */   
+  template<int index, typename Callback, typename... Args>
+  struct iterate_tuple 
+  {
+    static void next(std::tuple<Args...>& t, Callback callback) 
+    {
+      // Уменьшаем позицию и рекурсивно вызываем этот же метод 
+      iterate_tuple<index - 1, Callback, Args...>::next(t, callback);
+      std::cout << ",";
+      // Вызываем обработчик и передаем ему позицию и значение элемента
+      callback(index, std::get<index>(t));
+    }
+  };
+    
+  // Частичная специализация для индекса 0 (завершает рекурсию)
+  template<typename Callback, typename... Args>
+  struct iterate_tuple<0, Callback, Args...> 
+  {
+    static void next(std::tuple<Args...>& t, Callback callback) 
+    {
+      callback(0, std::get<0>(t));
+    }
+  };
+
+  // Частичная специализация для индекса -1 (пустой кортеж)
+  template<typename Callback, typename... Args>
+  struct iterate_tuple<-1, Callback, Args...>
+  {
+    static void next(std::tuple<Args...>& t, Callback callback)
+    {
+      // ничего не делаем
+    }
+  };
+}
+
+//
+// "Волшебный" print_tuple для обхода элементов кортежа (compile time!):
+//
+template<typename Callback, typename... Args>
+void print_tuple(std::tuple<Args...>& t, Callback callback) 
+{
+  // Размер кортежа
+  int const t_size = std::tuple_size<std::tuple<Args...>>::value;
+    
+  // Запускаем рекурсивный обход элементов кортежа во время компиляции
+  tpl::iterate_tuple<t_size - 1, Callback, Args...>::next(t, callback);
+}
+
+
+// Код функции print_tuple опущен для простоты примера 
+// (его лучше вынести в отдельный файл *.h)
+
+struct callback
+{
+  template<typename T>
+  void operator()(int index, T&& t) // index - это позиция элемента в кортеже
+  {                                 // t - значение элемента
+    print(t);
+  }
+};
+
+
+template<typename T>
+typename std::enable_if<type_is_tuple<T>(), void>::type
+print(T tuple)
+{
+   // Обход элементов кортежа и вызвов обработчика 
+   print_tuple(tuple, callback());
+}
+
+template<typename... Args>
+void print(Args... args)
+{
+   // Значения аргументов функции внутри кортежа 
+   auto tuple = std::make_tuple(std::forward<Args>(args)...);
+   
+   // Обход элементов кортежа и вызвов обработчика 
+   print_tuple(tuple, callback());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,53 @@
 int main()
 {
 
-  //Печать адреса как
+  //Печать адреса как char
+  std::cout << "char:\t";
   print(char(-1));
-  //Печать адреса как
+  std::cout << std::endl;
+  
+  //Печать адреса как short
+  std::cout << "short:\t";
   print(short(0));
-  //Печать адреса как
+  std::cout << std::endl;
+  
+  //Печать адреса как int
+  std::cout << "int:\t";
   print(int(2130706433));
-  //Печать адреса как
+  std::cout << std::endl;
+  
+  //Печать адреса как long
+  std::cout << "long:\t";
   print(long(8875824491850138409));
-  //Печать адреса как
-  ///std::string
-  //Печать адреса как
-  ///std::vector
-  //Печать адреса как
-  ///std::list
-  //Опционально. Печать адреса как
-  ///std::tuple
-
+  std::cout << std::endl;
+  
+  //Печать адреса как std::string
+  std::cout << "string:\t";
+  print(std::string{"Hello world!"});
+  std::cout << std::endl;
+  
+  //Печать адреса как std::vector
+  std::cout << "vector:\t";
+  std::vector<int> v{int(2130706433), int(2130706433), int(2130706433)};
+  print(v);
+  std::cout << std::endl;
+  
+  //Печать адреса как std::list
+  std::cout << "list:\t";
+  std::list<int> l{int(2130706433), int(2130706433), int(2130706433)};
+  print(l);
+  std::cout << std::endl;
+  
+  //Опционально. Печать адреса как std::tuple
+  print(std::make_tuple(char(-1), short(0), int(2130706433), long(8875824491850138409)));
+  std::cout << std::endl;
+  print(char(-1), 42, 100000);
+  std::cout << std::endl;
+    
+    
   // float
   print(1.2345);
-    
+  std::cout << std::endl;
 
   return 0;
 }
