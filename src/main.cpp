84230f3b7a85f287b8659826308cbb39efe20f55
Georgy Garnov <george.garnov@gmail.com>
2018-09-28 19:22:29 +0300

    comment vector and list

--- src/main.cpp
+++ src/main.cpp
@@ -31,17 +31,17 @@ int main()
   print(std::string{"Hello world!"});
   std::cout << std::endl;
   
-  //Печать адреса как std::vector
-  std::cout << "vector:\t";
-  std::vector<int> v{int(2130706433), int(2130706433), int(2130706433)};
-  print(v);
-  std::cout << std::endl;
+  // //Печать адреса как std::vector
+  // std::cout << "vector:\t";
+  // std::vector<int> v{int(2130706433), int(2130706433), int(2130706433)};
+  // print(v);
+  // std::cout << std::endl;
   
-  //Печать адреса как std::list
-  std::cout << "list:\t";
-  std::list<int> l{int(2130706433), int(2130706433), int(2130706433)};
-  print(l);
-  std::cout << std::endl;
+  // //Печать адреса как std::list
+  // std::cout << "list:\t";
+  // std::list<int> l{int(2130706433), int(2130706433), int(2130706433)};
+  // print(l);
+  // std::cout << std::endl;
   
   // //Опционально. Печать адреса как std::tuple
   // print(std::make_tuple(char(-1), short(0), int(2130706433), long(8875824491850138409)));
